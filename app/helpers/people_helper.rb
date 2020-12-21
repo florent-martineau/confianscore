0fb5fffe5288836cae87e3dc164b19a4f0ecc396
kjoannon <kevin.joannon@outlook.fr>
2020-12-21 14:46:33 +0100

    Styliz

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -8,4 +8,25 @@ module PeopleHelper
         end
         medias_content
     end
+
+    def gravite(source)
+        score = source.score
+        if score < -0.5
+            "Très mauvaise"
+        elsif score < 0
+            "Mauvaise"
+        elsif score < 0.5
+            "Positif"
+        else
+            "Très positif"
+        end
+    end
+
+    def show_source(source)
+        if source.length < 150
+            source
+        else
+            source.first(150) + "..."
+        end
+    end
 end
