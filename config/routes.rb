0ffc896c3f1f4149936e2626c852847dad908423
kjoannon <kevin.joannon@outlook.fr>
2020-12-22 09:23:42 +0100

    Add Charte

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   resources :sources
   resources :people
   root to: 'main#index'
+  get '/charte' => 'main#charte', as: 'charte'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
