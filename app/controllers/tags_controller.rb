44900330ddc4670de81bfef0f1872a986dfaf342
Kévin Joannon <kevin.joannon@outlook.fr>
2021-01-07 17:38:02 +0100

    Add charte

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -13,53 +13,53 @@ class TagsController < ApplicationController
   end
 
   # GET /tags/new
-  def new
-    @tag = Tag.new
-  end
+  # def new
+  #   @tag = Tag.new
+  # end
 
-  # GET /tags/1/edit
-  def edit
-  end
+  # # GET /tags/1/edit
+  # def edit
+  # end
 
   # POST /tags
   # POST /tags.json
-  def create
-    @tag = Tag.new(tag_params)
+  # def create
+  #   @tag = Tag.new(tag_params)
 
-    respond_to do |format|
-      if @tag.save
-        format.html { redirect_to @tag, notice: 'Tag was successfully created.' }
-        format.json { render :show, status: :created, location: @tag }
-      else
-        format.html { render :new }
-        format.json { render json: @tag.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #   respond_to do |format|
+  #     if @tag.save
+  #       format.html { redirect_to @tag, notice: 'Tag was successfully created.' }
+  #       format.json { render :show, status: :created, location: @tag }
+  #     else
+  #       format.html { render :new }
+  #       format.json { render json: @tag.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # PATCH/PUT /tags/1
   # PATCH/PUT /tags/1.json
-  def update
-    respond_to do |format|
-      if @tag.update(tag_params)
-        format.html { redirect_to @tag, notice: 'Tag was successfully updated.' }
-        format.json { render :show, status: :ok, location: @tag }
-      else
-        format.html { render :edit }
-        format.json { render json: @tag.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   respond_to do |format|
+  #     if @tag.update(tag_params)
+  #       format.html { redirect_to @tag, notice: 'Tag was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @tag }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @tag.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /tags/1
   # DELETE /tags/1.json
-  def destroy
-    @tag.destroy
-    respond_to do |format|
-      format.html { redirect_to tags_url, notice: 'Tag was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  # def destroy
+  #   @tag.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to tags_url, notice: 'Tag was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
