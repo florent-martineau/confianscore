29b7d58788c66e79e904b60f3d94eb8ad85c9ade
Kévin Joannon <kevin.joannon@outlook.fr>
2021-01-29 22:00:58 +0100

    Fix issue in search

--- app/models/person.rb
+++ app/models/person.rb
@@ -101,53 +101,60 @@ class Person < ApplicationRecord
     end
 
     def levenshtein_distance(t)
-        s = self.full_name&.downcase.to_s
-        m = s.length
-        n = t.length
-        return m if n == 0
-        return n if m == 0
-        d = Array.new(m+1) {Array.new(n+1)}
+        if self.full_name.present?
+          s = self.full_name&.downcase.to_s
+          m = s.length
+          n = t.length
+          return m if n == 0
+          return n if m == 0
+          d = Array.new(m+1) {Array.new(n+1)}
 
-        (0..m).each {|i| d[i][0] = i}
-        (0..n).each {|j| d[0][j] = j}
-        (1..n).each do |j|
-          (1..m).each do |i|
-            d[i][j] = if s[i-1] == t[j-1]  # adjust index into string
-                        d[i-1][j-1]       # no operation required
-                      else
-                        [ d[i-1][j]+1,    # deletion
-                          d[i][j-1]+1,    # insertion
-                          d[i-1][j-1]+1,  # substitution
-                        ].min
-                      end
+          (0..m).each {|i| d[i][0] = i}
+          (0..n).each {|j| d[0][j] = j}
+          (1..n).each do |j|
+            (1..m).each do |i|
+              d[i][j] = if s[i-1] == t[j-1]  # adjust index into string
+                          d[i-1][j-1]       # no operation required
+                        else
+                          [ d[i-1][j]+1,    # deletion
+                            d[i][j-1]+1,    # insertion
+                            d[i-1][j-1]+1,  # substitution
+                          ].min
+                        end
+            end
           end
+          full_name_distance = d[m][n]
         end
-        full_name_distance = d[m][n]
+        full_name_distance ||= 100000
 
-        s = self.nickname&.downcase.to_s
-        m = s.length
-        n = t.length
-        return m if n == 0
-        return n if m == 0
-        d = Array.new(m+1) {Array.new(n+1)}
+        if self.nickname.present?
+          s = self.nickname&.downcase.to_s
+          m = s.length
+          n = t.length
+          return m if n == 0
+          return n if m == 0
+          d = Array.new(m+1) {Array.new(n+1)}
 
-        (0..m).each {|i| d[i][0] = i}
-        (0..n).each {|j| d[0][j] = j}
-        (1..n).each do |j|
-          (1..m).each do |i|
-            d[i][j] = if s[i-1] == t[j-1]  # adjust index into string
-                        d[i-1][j-1]       # no operation required
-                      else
-                        [ d[i-1][j]+1,    # deletion
-                          d[i][j-1]+1,    # insertion
-                          d[i-1][j-1]+1,  # substitution
-                        ].min
-                      end
+          (0..m).each {|i| d[i][0] = i}
+          (0..n).each {|j| d[0][j] = j}
+          (1..n).each do |j|
+            (1..m).each do |i|
+              d[i][j] = if s[i-1] == t[j-1]  # adjust index into string
+                          d[i-1][j-1]       # no operation required
+                        else
+                          [ d[i-1][j]+1,    # deletion
+                            d[i][j-1]+1,    # insertion
+                            d[i-1][j-1]+1,  # substitution
+                          ].min
+                        end
+            end
           end
+          nick_name_distance = d[m][n]
         end
-        nick_name_distance = d[m][n]
+        nick_name_distance ||= 10000
 
         [full_name_distance, nick_name_distance].min
+
     end
 
     def reset_score
