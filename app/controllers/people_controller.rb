1aabc54021304568bfbb1c0f15695f9c0c52b866
Kévin Joannon <kevin.joannon@outlook.fr>
2021-02-24 07:56:58 +0100

    Secure person

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -3,9 +3,9 @@ class PeopleController < ApplicationController
 
   # GET /people
   # GET /people.json
-  def index
-    @people = Person.all
-  end
+  # def index
+  #   @people = Person.all
+  # end
 
   # GET /people/1
   # GET /people/1.json
@@ -18,8 +18,8 @@ class PeopleController < ApplicationController
   end
 
   # GET /people/1/edit
-  def edit
-  end
+  # def edit
+  # end
 
   # POST /people
   # POST /people.json
@@ -45,18 +45,18 @@ class PeopleController < ApplicationController
 
   # PATCH/PUT /people/1
   # PATCH/PUT /people/1.json
-  def update
-    respond_to do |format|
-      @person.media = @person.define_media(person_params)
-      if @person.save
-        format.html { redirect_to @person, notice: 'Person was successfully updated.' }
-        format.json { render :show, status: :ok, location: @person }
-      else
-        format.html { render :edit }
-        format.json { render json: @person.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   respond_to do |format|
+  #     @person.media = @person.define_media(person_params)
+  #     if @person.save
+  #       format.html { redirect_to @person, notice: 'Person was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @person }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @person.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /people/1
   # DELETE /people/1.json
