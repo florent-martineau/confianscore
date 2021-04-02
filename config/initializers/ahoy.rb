a9522146693ce990d6bda7e01ef25e02b14455de
kjdelys <kevin.joannon@outlook.fr>
2021-04-02 18:28:12 +0200

    Mask ip

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -1,5 +1,10 @@
 class Ahoy::Store < Ahoy::DatabaseStore
+  def authenticate(data)
+    # disables automatic linking of visits and users
+  end
 end
 
 # set to true for JavaScript tracking
 Ahoy.api = false
+Ahoy.mask_ips = true
+Ahoy.cookies = false
