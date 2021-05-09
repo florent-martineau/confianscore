de5dfd1f297ae626410323726a8a4c06034e2ed1
Kévin Joannon <kevin.joannon@outlook.fr>
2021-05-09 16:59:06 +0200

    Add helper prediction

--- app/helpers/predictions_helper.rb
+++ app/helpers/predictions_helper.rb
@@ -2,7 +2,7 @@ module PredictionsHelper
     def text_n_status(prediction)
         status = prediction.has_been_confirmed
         if status.nil?
-            status_as_text = "En attente"
+            status_as_text = "Prédiction en attente"
         elsif status == false
             status_as_text = "Prédiction fausse"
         elsif status == true
