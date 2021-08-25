8a06e83cc1820c1c27d37ee2fb3ef49745caa4cd
kjdelys <kevin.joannon@outlook.fr>
2021-08-25 15:32:04 +0200

    Fix issue database no secret_key

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'mechanize'
 gem 'twitter'
 gem 'ahoy_matey'
 gem 'devise'
+gem "figaro"
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module Confianscore
     config.load_defaults 6.0
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
     config.middleware.use Rack::Deflater
+
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
